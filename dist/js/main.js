d51fff710c27fde179dc1c4fc3f491fbf7eac425
Stepan Zverev <stepanzverev@gmail.com>
2020-10-10 15:33:45 +0300

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,13 @@
+// document.getElementById("video").disablePictureInPicture = true;
+
+$('.slider').slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 300,
+    slidesToShow: 3,
+    centerMode: false,
+    variableWidth: true,
+    arrows: false,
+  });
