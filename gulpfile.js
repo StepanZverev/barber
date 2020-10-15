1dc25402061ecef976c1455499962d9c71ae14ab
Stepan Zverev <stepanzverev@gmail.com>
2020-10-15 14:25:49 +0300

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,6 @@ gulp.task('scss', function(){
 gulp.task('css', function(){
   return gulp.src([
     'node_modules/normalize.css/normalize.css',
-    'node_modules/slick-carousel/slick/slick.css',
     'node_modules/magnific-popup/dist/magnific-popup.css'
   ])
     .pipe(concat('_libs.scss'))
@@ -46,7 +45,6 @@ gulp.task('script', function(){
 
 gulp.task('js', function(){
   return gulp.src([
-    'node_modules/slick-carousel/slick/slick.js',
     'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
   ])
     .pipe(concat('libs.min.js'))
